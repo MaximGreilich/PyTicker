c46fcaf5bc5f684b47a81533ede7bc13aef06d53
MaximGreilich <mxgrei@gmail.com>
2025-11-20 16:45:22 +0100

    fix: Final definitive removal of all exercise folders and Test.py

--- Übung 2/util_module.py
+++
@@ -1,8 +0,0 @@
-#grma1075 94731
-def compute_area(width, height): 
-    return width * height
-
-class Configuration: 
-    @classmethod
-    def get_default_settings(cls):
-        return {'theme': 'dark', 'font-size': 12}
